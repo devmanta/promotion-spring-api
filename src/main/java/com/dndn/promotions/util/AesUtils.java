20dbcc75781522275450fa5d49695439660b6ad9
KimHeeJin <dlwlrgkwk82@gmail.com>
2022-11-27 01:15:01 +0900

    add Aes encryption/decryption

---
+++ src/main/java/com/dndn/promotions/util/AesUtils.java
@@ -0,0 +1,47 @@
+package com.dndn.promotions.util;
+
+import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+@Component
+public class AesUtils {
+    //https://www.devglan.com/online-tools/jasypt-online-encryption-decryption
+
+    private final String alg = "AES/CBC/PKCS5Padding";
+    @Value("${dndn.aes.key}")
+    private String key;
+//    private final String key = "abcdefghabcdefghabcdefghabcdefgh"; // 32byte
+
+    @Value("${dndn.aes.iv}")
+    private String iv;
+//    private String iv = "0123456789abcdef"; // 16byte
+
+    // 암호화
+    public String encryptAES256(String text) throws Exception {
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] encrypted = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    // 복호화
+    public String decryptAES256(String cipherText) throws Exception {
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+        byte[] decrypted = cipher.doFinal(decodedBytes);
+        return new String(decrypted, StandardCharsets.UTF_8);
+    }
+
+}
